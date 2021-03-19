1695da525b643661e4ad45ddb26d4dd1cecb139c
Jiawei Zheng <zjwdnlm93716@gmail.com>
2021-03-19 11:28:54 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: '自家小仓库',
+    description: '技术文档',
+    themeConfig: {
+        logo: '/assets/imgs/logo.png',
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '前端',
+                items: [
+                    {text: 'Javascript', link: '/Javascript/'}
+                ]
+            },
+            { text: 'External', link: 'http://jiawei.space' },
+        ]
+    },
+    plugins: {
+        "vuepress-plugin-auto-sidebar": {}
+    }
+}
